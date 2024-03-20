f3b02cba30c16d4a2ca382e70ca5318e988fdf5b
Vivek Kumar <er_vivekkumar@outlook.com>
2024-03-20 23:20:00 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="er-vivekkumar",
+    author_email="er_vivekkumar@outlook.com",
+    packages=find_packages()
+)
